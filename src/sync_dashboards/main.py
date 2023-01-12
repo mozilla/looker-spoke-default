4d060b722b1c767c52965cdab34539d9c48f26a4
Frank Bertsch <fbertsch@mozilla.com>
2023-01-12 08:51:19 -0500

    Quadruple timeout
    Addresses https://github.com/mozilla/lookml-generator/issues/592

--- src/sync_dashboards/main.py
+++ src/sync_dashboards/main.py
@@ -48,7 +48,7 @@ def get_all_linked_dashboards(sdk: methods.Looker31SDK) -> dict:
     """
     remote_config = {}
 
-    transport_options = looker_sdk.rtl.transport.TransportOptions({"timeout": 60 * 5})
+    transport_options = looker_sdk.rtl.transport.TransportOptions({"timeout": 60 * 20})
     dashboards = sdk.search_dashboards(deleted=False, transport_options=transport_options)
 
     for dashboard in dashboards:
